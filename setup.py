cd79ee29b673ec6827f24ca5ab35feabdb29ef48
Michael Ford <mford@sfu.ca>
2020-08-19 15:53:03 -0400

    Modify setup and configuration files

--- setup.py
+++ setup.py
@@ -1,24 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-    Setup file for immunotyper.
-    Use setup.cfg to configure your project.
+from setuptools import find_packages, setup
 
-    This file was generated with PyScaffold 3.2.3.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
-"""
-import sys
-
-from pkg_resources import VersionConflict, require
-from setuptools import setup
-
-# try:
-#     require('setuptools>=38.3')
-# except VersionConflict:
-#     print("Error: version of setuptools is too old (<38.3)!")
-#     sys.exit(1)
-
-
-if __name__ == "__main__":
-    # setup(use_pyscaffold=True)
-    setup(use_pyscaffold=False)
+setup(
+    name='immunotyper',
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    version='0.1.0',
+    description='IGHV genotyper tool using PacBio WGS Long Reads',
+    author='Michael Ford',
+    license='GNU General Public License v3 (GPL-3) modified by the Commons Clause',
+)
